40047a379a3e7bc0c81eb34b1fda6f651b373de2
Gabriel Santiago <gaaasts@gmail.com>
2022-09-15 10:40:28 -0300

    first commit

---
+++ turtle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  turtle
+//
+//  Created by Gabriel Santiago on 15/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
